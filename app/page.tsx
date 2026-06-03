8499bb1ea81d816d538c4f4529aa89e69793a1d9
Smurf1242 <98929187+Smurf1242@users.noreply.github.com>
2026-06-03 08:30:44 +0100

    photoresize 2

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default async function Home() {
       <img 
         src="/logo.png" 
         alt="ClipFlow AI" 
-        className="h-18 w-auto"
+        className="h-12 w-auto"
       />
     </div>
 
